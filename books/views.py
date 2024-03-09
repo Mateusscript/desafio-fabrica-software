b5bbcf4c8e1e86ee7fc19cec8e8fa2594719145f
Mateusscript <mateusgamesgff@gmail.com>
2024-03-09 20:18:04 -0300

    interação book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.response import Response
+from books.serializers import BookSerializer
+from .models import Book
+import requests
 
-# Create your views here.
+class BookListAPIView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
